92f1bad7d4520af0b3e554464b788733542c349e
Unknown <brunofreitas5.25@hotmail.com>
2018-10-13 01:19:42 +0100

    Dynamic Texture

--- Final Descent/Assets/Scripts/DynamicTexture.cs
+++ Final Descent/Assets/Scripts/DynamicTexture.cs
@@ -2,28 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DynamicTexture : MonoBehaviour {
-	Material mat;
+public class DynamicTexture : MonoBehaviour
+{
+    public Color ColorShip1 = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+    public Color ColorShip2 = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+    public Color ColorShip3 = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+    Texture2D texture;
+    void Start()
+    {
 
-	public Color ColorWindow = new Color(0.0f, 0.0f, 0.0f, 0.0f);
-	public Color ColorShip = new Color(0.0f, 0.0f, 0.0f, 0.0f);
-	Texture2D texture;
-	void Start () 
-	{
-		mat = GetComponent<Renderer>().material;
-		texture = new Texture2D(2, 2, TextureFormat.RGBA32, false, false);	
-		texture.SetPixel(0, 0, ColorWindow);
-		texture.SetPixel(0, 1, ColorShip);
-		texture.filterMode = FilterMode.Point;
-		texture.Apply();
-		mat.SetTexture("_MainTex", texture);
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		texture.SetPixel(0, 0, ColorWindow);
-		texture.SetPixel(0, 1, ColorShip);
-		texture.Apply();
-	}
+        texture = new Texture2D(2, 2, TextureFormat.RGBA32, false, false);
+        texture.SetPixel(0, 1, ColorShip1);
+        texture.SetPixel(1, 1, ColorShip2);
+        texture.SetPixel(0, 0, ColorShip3);
+        texture.SetPixel(1, 0, ColorShip3);
+        texture.filterMode = FilterMode.Point;
+        texture.Apply();
+        foreach (Renderer r in GetComponentsInChildren<Renderer>())
+            r.material.SetTexture("_Texture2D_BaseColors", texture);
+    }
+
+    void Update()
+    {
+        texture.SetPixel(0, 1, ColorShip1);
+        texture.SetPixel(1, 1, ColorShip2);
+        texture.SetPixel(0, 0, ColorShip3);
+        texture.SetPixel(1, 0, ColorShip3);
+        texture.Apply();
+    }
 }
