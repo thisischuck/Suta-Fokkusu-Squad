2c75615329c311e97be0934a1c928bef1c8aee65
Unknown <washinima99@gmail.com>
2018-10-30 12:45:39 +0000

    New Gun Shit. Needs Testing

---
+++ Final Descent/Assets/Scripts/Weapon Scripts/Ps GunManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PsGunManager : MonoBehaviour
+{
+    private ParticleSystem system;
+
+    bool canFire;
+    public int bulletsPerClick = 1;
+    public float fireRate = 0.45f; //segundos
+
+    // Use this for initialization
+    void Start()
+    {
+        system = this.gameObject.GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && canFire)
+        {
+            system.Emit(bulletsPerClick);
+            StartCoroutine(FireRateIE());
+        }
+    }
+
+    IEnumerator FireRateIE()
+    {
+        canFire = false;
+        yield return new WaitForSeconds(fireRate);
+        canFire = true;
+    }
+}
---
+++ Final Descent/Assets/Scripts/Weapon Scripts/Ps WeaponSwitching.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PsWeaponSwitching : MonoBehaviour
+{
+    public List<ParticleSystem> Weapons;
+
+    int previousWeapon, selectedWeapon;
+
+    // Use this for initialization
+    void Start()
+    {
+        foreach (ParticleSystem a in Weapons)
+        {
+            a.gameObject.SetActive(false);
+        }
+        Weapons[0].gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        previousWeapon = selectedWeapon;
+
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            selectedWeapon = 0;
+        }
+        else if (Input.GetKey(KeyCode.Alpha2))
+        {
+            selectedWeapon = 1;
+        }
+        else if (Input.GetKey(KeyCode.Alpha3))
+        {
+            selectedWeapon = 2;
+        }
+        else if (Input.GetKey(KeyCode.Alpha4))
+        {
+            selectedWeapon = 3;
+        }
+
+        //Only if we change weapons we call the SelectWapon() function to update which weapon is being used and activate it
+        if (previousWeapon != selectedWeapon)
+        {
+            SelectWeapon();
+        }
+    }
+
+
+    void SelectWeapon()
+    {
+        foreach (ParticleSystem a in Weapons)
+        {
+            a.gameObject.SetActive(false);
+        }
+
+        Weapons[selectedWeapon].gameObject.SetActive(true);
+    }
+
+
+}
+
