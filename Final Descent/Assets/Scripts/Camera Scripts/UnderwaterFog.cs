1b9fab6fb619dfb85ab2e7243963e664140c9a3e
Unknown <brunofreitas5.25@hotmail.com>
2018-11-12 12:09:54 +0000

    Water stuff

---
+++ Final Descent/Assets/Scripts/Camera Scripts/UnderwaterFog.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class UnderwaterFog : MonoBehaviour
+{
+    public Transform waterPlane;
+    public PostProcessVolume postProcessVolume;
+    public PostProcessProfile postProcessUnderwater;
+    public PostProcessProfile postProcessDefault;
+    private bool underwater;
+    private bool defaultFog = false;
+    private Color defaultFogColor = Color.black;
+    private float defaultFogDensity = 0;
+
+    public Color fogColor = Color.blue;
+    public float density = 0.075f;
+    void Start()
+    {
+        defaultFog = RenderSettings.fog;
+        defaultFogColor = RenderSettings.fogColor;
+        defaultFogDensity = RenderSettings.fogDensity;
+    }
+
+    void Update()
+    {
+        if (waterPlane == null) return;
+
+        underwater = transform.position.y < waterPlane.position.y ? true : false;
+        SetFog();
+    }
+
+    void SetFog()
+    {
+        RenderSettings.fog = underwater ? true : defaultFog;
+        RenderSettings.fogColor = underwater ? fogColor : defaultFogColor;
+        RenderSettings.fogDensity = underwater ? density : defaultFogDensity;
+        postProcessVolume.profile = underwater ? postProcessUnderwater : postProcessDefault;
+    }
+}
