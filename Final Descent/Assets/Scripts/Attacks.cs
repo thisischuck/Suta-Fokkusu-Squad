2ac8ec4c7400dd83f77438e0a29e0ef61f070b45
Unknown <marciosr@live.com.pt>
2018-11-15 10:02:33 +0000

    Auto stash before merge of "newmaster" and "origin/newmaster"

--- Final Descent/Assets/Scripts/Attacks.cs
+++ Final Descent/Assets/Scripts/Attacks.cs
@@ -4,37 +4,41 @@ using UnityEngine;
 
 public class Attacks : MonoBehaviour {
 
-    List<GameObject> _playerList;
+    List<Transform> _playerList;
+    public Transform target;
+    public float force;
 
 	// Use this for initialization
 	void Start () {
 
-        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        /*GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
 
         foreach (GameObject player in players)
         {
-            _playerList.Add(player);
-        }
+            _playerList.Add(player.gameObject.transform);
+        }*/
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKey(KeyCode.P))
         {
-            foreach(GameObject player in _playerList)
+            /*foreach(Transform player in _playerList)
             {
                 GetSucked(player);
-            }
+            }*/
+
+            GetSucked(target);
         }
 	}
 
-    void GetSucked(GameObject player)
+    void GetSucked(Transform player)
     {
         Vector3 direction = Vector3.zero;
-        if(Vector3.Distance(transform.position, player.transform.position) < 20f)
+        if(Vector3.Distance(transform.position, player.position) < 20f)
         {
-            direction = player.transform.position - transform.position;
-            player.transform.position = Vector3.MoveTowards(player.transform.position, transform.position, 20);
+            direction = player.position - transform.position;
+            player.position = Vector3.MoveTowards(player.position, transform.position, force);
         }
     }
 }
