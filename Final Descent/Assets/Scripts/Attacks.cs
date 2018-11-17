aea35e9f12dbb42157c48793ed03165adcfe1be4
Unknown <marciosr@live.com.pt>
2018-11-17 17:57:55 +0000

    FDS JAH CRLH :rage:

--- Final Descent/Assets/Scripts/Attacks.cs
+++ Final Descent/Assets/Scripts/Attacks.cs
@@ -7,17 +7,34 @@ public class Attacks : MonoBehaviour {
     List<Transform> _playerList;
     public Transform target;
     public float force;
+    public float eelSpeed;
     public ParticleSystem ee;
 
     private float nextExplosion;
     private bool isExploding = false;
     private int explosionCount;
 
+    List<Transform> holes = new List<Transform>();
+    private bool isHighSpeed = false;
+    private bool isOnHole = false;
+    int nextHole;
+    Vector3 direction;
+
     // Use this for initialization
     void Start () {
 
         nextExplosion = 0f;
         explosionCount = 0;
+        eelSpeed = 15f;
+
+        //Look for the holes and Add'em to the lsit
+        GameObject holeSystem = GameObject.Find("Holes");
+
+        foreach (Transform child in holeSystem.transform)
+        {
+            holes.Add(child);
+        }
+        nextHole = Random.Range(0, holes.Count - 1);
 
         /*GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
 
@@ -37,7 +54,8 @@ public class Attacks : MonoBehaviour {
             }*/
 
             //GetSucked(target);
-            isExploding = !isExploding;
+            //isExploding = !isExploding;
+            isHighSpeed = !isHighSpeed;
         }
 
         if (isExploding && explosionCount < 3)
@@ -54,6 +72,11 @@ public class Attacks : MonoBehaviour {
             isExploding = false;
             explosionCount = 0;
         }
+
+        if (isHighSpeed)
+        {
+            HighSpeed(nextHole);
+        }
     }
 
     void GetSucked(Transform player)
@@ -64,5 +87,21 @@ public class Attacks : MonoBehaviour {
             direction = player.position - transform.position;
             player.position = Vector3.MoveTowards(player.position, transform.position, force);
         }
+    }
+
+    void HighSpeed(int hole)
+    {
+        if (Vector3.Distance(transform.position, holes[hole].position) < 1f)
+        {
+            //Look for another hole
+            nextHole = Random.Range(0, holes.Count);
+        }
+
+        direction = transform.position - holes[hole].position;
+        Quaternion targetRot = Quaternion.LookRotation(-direction, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRot, 8f);
+        //transform.forward = -direction;
+        transform.position = Vector3.MoveTowards(transform.position, holes[hole].position, Time.deltaTime * eelSpeed);
+        
     }
 }
