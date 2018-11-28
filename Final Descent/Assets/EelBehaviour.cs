e39056fbd9a3ccacd367b04ee13067d15e42ed41
Unknown <marciosr@live.com.pt>
2018-11-28 21:12:37 +0000

    oof

---
+++ Final Descent/Assets/EelBehaviour.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EelBehaviour : MonoBehaviour
+{
+    private StateMachine stateMachine;
+    private BaseStats baseStats;
+	private EelAttacks eelAttacks;
+
+	public bool alive;
+/*    public bool isTornado;
+    public bool isShockWave;
+    public bool isHighSpeed;
+    public bool isExploding;
+    public bool isCalling;
+    public bool isCharging;
+    public bool isBiting;*/
+
+    private float eelHealth;
+	private float eelMaxHealth;
+
+    void Start()
+    {
+        baseStats = GetComponent<BaseStats>();
+		eelAttacks = GetComponent<EelAttacks>();
+		eelHealth = baseStats.health;
+		eelMaxHealth = eelHealth;
+
+        //Actions
+        Action a_active = () => { alive = true; eelAttacks.Start(); };
+        Action a_onEntryTornado = () => { eelAttacks.OnEntryTornado(); };
+		Action a_tornado = () => { eelAttacks.Tornado(); };
+		Action a_onEntryHighSpeed = () => { eelAttacks.OnEntryHighSpeed(); };
+		Action a_highSpeed = () => {  eelAttacks.HighSpeed(); };
+		Action a_onEntryExplode = () => { eelAttacks.OnEntryExplode(); };
+		Action a_explode = () => { eelAttacks.Explode(); };
+		Action a_onEntryShockWave = () => { eelAttacks.OnEntryShockWave(); };
+		Action a_shockWave = () => { eelAttacks.ShockWave(); };
+		Action a_onEntryCalling = () => { eelAttacks.OnEntryCalling(); };
+		Action a_call = () => { eelAttacks.Calling(); };
+		Action a_onEntryCharge = () => { eelAttacks.OnEntryCharge(); };
+		Action a_charge = () => { eelAttacks.Charge(); };
+		Action a_onEntryBite = () => { eelAttacks.OnEntryBite(); };
+		Action a_bite = () => { eelAttacks.Bite(); };
+        Action a_dead = () => { alive = false; };
+
+        //Nodes
+        StateMachine_Node n_active = new StateMachine_Node("Active", new List<Action> { a_active },null, null);
+        StateMachine_Node n_tornado = new StateMachine_Node("Tornado", new List<Action> { a_tornado }, new List<Action> { a_onEntryTornado }, null);
+        StateMachine_Node n_highSpeed = new StateMachine_Node("HighSpeed", new List<Action> { a_highSpeed }, new List<Action> { a_onEntryHighSpeed }, null);
+        StateMachine_Node n_shockWave = new StateMachine_Node("ShockWave", new List<Action> { a_shockWave }, null, null);
+        StateMachine_Node n_explode = new StateMachine_Node("Exploding", new List<Action> { a_explode }, null, null);
+        StateMachine_Node n_call = new StateMachine_Node("Calling", new List<Action> { a_call }, null, null);
+        StateMachine_Node n_Charge = new StateMachine_Node("Charging", new List<Action> { a_charge }, new List<Action> { a_onEntryCharge }, null);
+        StateMachine_Node n_Bite = new StateMachine_Node("Biting", new List<Action> { a_bite }, new List<Action> { a_onEntryBite }, null);
+        StateMachine_Node n_dead = new StateMachine_Node("Dead", null, null, null); //ainda sem açao
+
+		//Transitions
+		StateMachine_Transition t_ativeToHighSpeed = new StateMachine_Transition("Active To HighSpeed", () => { return alive == true; }, n_highSpeed,
+			null);
+		StateMachine_Transition t_highSpeedToTornado = new StateMachine_Transition("HighSpeed To Tornado", () => { return eelAttacks.isHighSpeed == false && 
+			eelAttacks.MedPlayersPositions(transform.position) > 50f; }, n_tornado, null); //Player is far away, tornado is activated
+        StateMachine_Transition t_highSpeedToShockwave = new StateMachine_Transition("HighSpeed To ShockWave", () => { return eelAttacks.isHighSpeed == false &&
+			eelAttacks.MedPlayersPositions(transform.position) < 50; }, n_shockWave, null);
+        StateMachine_Transition t_tornadoToExplode = new StateMachine_Transition("Tornado To Explode", () => { return eelAttacks.isTornado == false && eelAttacks.goToExplode == true; }, n_explode,
+            null);
+        StateMachine_Transition t_explodeToHighSpeed = new StateMachine_Transition("Explode To HighSpeed", () => { return eelAttacks.isExploding == false && eelAttacks.RandNum() == 0; }, n_highSpeed,
+            null);
+        StateMachine_Transition t_explodeToCharge = new StateMachine_Transition("Explode To Charge", () => { return eelAttacks.isExploding == false && eelAttacks.RandNum() == 1; }, n_Charge,
+            null);
+        StateMachine_Transition t_shockWaveToCharge = new StateMachine_Transition("ShockWave To Charge", () => { return eelAttacks.isShockWave == false && eelAttacks.RandNum() == 1; }, n_Charge,
+            null);
+        StateMachine_Transition t_shockWaveToHighSpeed = new StateMachine_Transition("ShockWave To HighSpeed", () => { return eelAttacks.isShockWave == false && eelAttacks.RandNum() == 0; }, n_highSpeed,
+            null);
+        StateMachine_Transition t_callingToCharge = new StateMachine_Transition(" Calling To Charge", () => { return eelAttacks.isCalling == false && eelAttacks.RandNum() == 1; }, n_Charge,
+            null);
+        StateMachine_Transition t_callingToTornado = new StateMachine_Transition("Calling To Tornado", () => { return eelAttacks.isCalling == false && eelAttacks.RandNum() == 0; }, n_tornado,
+            null);
+        StateMachine_Transition t_biteToHighSpeed = new StateMachine_Transition("Bite To HighSpeed", () => { return GoToHighSpeed() == true; }, n_highSpeed,
+            null); //CODICAO: Se a Eel receber mais de 5% da vida desde q começou a atacar(charge) volta para o HighSpeed node
+		StateMachine_Transition t_biteToCharge = new StateMachine_Transition("Bite To Charge", () => { return GoToHighSpeed() == false; }, n_Charge,
+			null); //CODICAO: Se a Eel receber menos de 5% da vida desde q começou a atacar(charge) volta para o Charge node
+		StateMachine_Transition t_anyToBite = new StateMachine_Transition("Bite", () => { return eelAttacks.isBiting == true; }, n_Bite,
+			null); //CODICAO: Se alguem estiver em range de morder irá transicionar para Bite node
+		StateMachine_Transition t_anyCalling = new StateMachine_Transition("Calling", () => { return activateCall() == true; }, n_call,
+			null);
+		StateMachine_Transition t_killed = new StateMachine_Transition("Killed", () => { return alive == false; }, n_dead, null); //Eel gets killed
+
+		//Active
+		n_active.AddTransition(t_ativeToHighSpeed, t_killed, t_anyCalling, t_anyToBite);
+
+		//HighSpeed
+		n_highSpeed.AddTransition(t_highSpeedToTornado, t_highSpeedToShockwave,t_killed, t_anyCalling, t_anyToBite);
+
+		//Tornado
+		n_tornado.AddTransition(t_tornadoToExplode, t_killed, t_anyCalling, t_anyToBite);
+
+		//Explode
+		n_explode.AddTransition(t_explodeToCharge, t_killed, t_explodeToHighSpeed, t_anyCalling, t_anyToBite);
+
+		//ShockWave
+		n_shockWave.AddTransition(t_shockWaveToCharge, t_shockWaveToHighSpeed, t_killed, t_anyCalling, t_anyToBite);
+
+		//Bite
+		n_Bite.AddTransition(t_biteToCharge, t_biteToHighSpeed, t_killed, t_anyCalling);
+
+		//Call
+		n_call.AddTransition(t_callingToCharge, t_callingToTornado, t_killed, t_anyToBite);
+
+        stateMachine = new StateMachine(n_active);
+    }
+
+    void Update()
+    {
+        if (alive)
+        {
+            List<Action> actions = stateMachine.Run();
+            if (actions != null)
+            {
+                foreach (var a in actions)
+                {
+                    if (a != null)
+                    {
+                        a.Invoke();
+						Debug.Log(stateMachine.currentNode.name);
+                    }
+                }
+            }
+        }
+		if (eelHealth <= 0)
+			alive = false;
+		else alive = true;
+    }
+
+	public bool activateCall()
+	{
+		if (eelHealth == eelMaxHealth / 0.5)//vida a 50%
+			return true;
+		else if (eelHealth == eelMaxHealth * 0.75)//vida a 75%
+			return true;
+		else if (eelHealth == eelMaxHealth * 0.25)//vida a 25%
+			return true;
+		else return false;
+	}
+
+	public bool GoToHighSpeed()
+	{
+		if (eelAttacks.currentHealth - eelHealth >= eelHealth * 0.05)
+		{
+			//eelAttacks.isHighSpeed = true;
+			eelAttacks.sameCharge = false;
+			return true;
+		}
+		else
+		{
+			//eelAttacks.isCharging = true;
+			eelAttacks.sameCharge = true;
+			return false;
+		}
+	}
+}
---
+++ Final Descent/Assets/Scripts/EelAttacks.cs
@@ -0,0 +1,494 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EelAttacks : MonoBehaviour {
+
+    private List<Transform> _playerList;
+    public float eelSpeed;
+	//public float eelRotation;
+
+    //Tornado
+    public bool isTornado;
+    private Vector3 centerPosition;
+    public float radius;
+    public float force;
+	private float timeTornading;
+	private float TornadoDuration;
+	public bool goToExplode;
+
+    //EletricExplosion
+    private float nextExplosion;
+    public bool isExploding;
+    private int explosionCount;
+    public ParticleSystem ee;
+
+    //highspeed
+    private List<Transform> RHoles;
+    private List<Transform> LHoles;
+    private Transform selectedHole;
+    private bool left;
+    private bool goingIn;
+    public bool isHighSpeed;
+    private int nextHole;
+    private Vector3 direction;
+	private int voltasCount;
+	private int voltas;
+	public int randVoltas;
+	public bool endHS;
+	public Transform caveCenter;
+
+    //calling
+    public GameObject enemyBabies;
+    private int enemiesPerHole;
+	public bool isCalling;
+
+	//Charge
+	public bool isCharging;
+	public float currentHealth;
+	public bool sameCharge;
+	private int playerIndex;
+
+	//Bite
+	public Transform BottomMouth;
+    public bool isBiting;
+    private Quaternion openMouth;
+    private Quaternion closeMouth;
+    private bool mouthClosed;
+	private Transform inBiteRange;
+
+	//ShowWave
+	public bool isShockWave;
+	public ParticleSystem SW;
+	private int waveCount;
+	private float nextWave;
+
+	//Damage Variables
+	public float ExplosionDamage;
+	public float BiteDamage;
+
+	// Use this for initialization
+	public void Start () {
+		isTornado = false;
+		TornadoDuration = 5f;
+		goToExplode = false;
+		isExploding = false;
+		RHoles = new List<Transform>();
+		LHoles = new List<Transform>();
+		goingIn = false;
+		isHighSpeed = false;
+		enemiesPerHole = 3;
+		isCalling = false;
+		isCharging = false;
+		sameCharge = true;
+		isBiting = false;
+		isShockWave = false;
+
+        nextExplosion = 0f;
+        explosionCount = 0;
+        eelSpeed = 15f;
+        radius = 20f;
+		timeTornading = 0.0f;
+		nextWave = 0.0f;
+
+        _playerList = new List<Transform>();
+
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject player in players)
+        {
+            _playerList.Add(player.gameObject.transform);
+        }
+
+        AddHoles();
+
+		openMouth = Quaternion.Euler(BottomMouth.rotation.y + 20.0f, BottomMouth.rotation.y, BottomMouth.rotation.z);
+		closeMouth = BottomMouth.rotation;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			//isTornado = !isTornado;
+			//PrepTornado();
+			//isExploding = !isExploding;
+			//isHighSpeed = !isHighSpeed;
+			//OnEntryHighSpeed();
+			//Calling();
+			//isBiting = !isBiting;
+			//OnEntryBite();
+			//isShockWave = !isShockWave;
+			isCharging = !isCharging;
+			OnEntryCharge();
+		}
+
+		if (isExploding)
+		{
+			Explode();
+		}
+
+		if (isTornado)
+		{
+			if (Time.time >= timeTornading)
+				isTornado = false;
+			else
+				Tornado();
+		}
+		if (timeTornading >= 5f) isTornado = false;
+
+		if (isHighSpeed)
+		{
+			HighSpeed();
+		}
+
+		if (isCharging)
+			Charge();
+
+		if (isBiting)
+			Bite();
+	}
+
+	#region Tornado
+	public void OnEntryTornado()
+    {
+		Debug.Log("OnEntryTornado");
+
+		isTornado = true;
+		timeTornading = Time.time + TornadoDuration;
+        centerPosition = transform.position + Vector3.forward * 10f;
+		goToExplode = false;
+    }
+
+	/*public void OnExitTornado()
+	{
+		Debug.Log("OnExitTornado");
+		transform.position = Vector3.MoveTowards(transform.position, centerPosition, eelSpeed * Time.deltaTime);
+		goToExplode = true;
+	}*/
+
+	public void Tornado()// colocar a dar damage stun qunado toca na ragdoll da enguia
+    {
+		if(!isTornado)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, centerPosition, eelSpeed * Time.deltaTime);
+			if (Vector3.Distance(transform.position, centerPosition) <= 3f)
+				goToExplode = true;
+		}
+		if (Time.time >= timeTornading)
+		{
+			isTornado = false;
+			return;
+		}
+
+		//CircularMotion //Still needs adjusting
+		float rotSpeed = 80f;
+
+        transform.RotateAround(centerPosition, Vector3.up, Time.deltaTime * rotSpeed);
+        Vector3 desiredPosition = (transform.position - centerPosition).normalized * radius + centerPosition;
+        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, 5f * Time.deltaTime);
+
+		//GetSucked()
+        foreach(Transform player in _playerList)
+        {
+            Vector3 direction = Vector3.zero;
+            if (Vector3.Distance(centerPosition, player.position) < 50f)
+            {
+                direction = player.position - transform.position;
+                player.position = Vector3.MoveTowards(player.position, centerPosition, force);
+            }
+        }
+    }
+	#endregion
+
+	#region Explode
+
+	public void OnEntryExplode()
+	{
+		isExploding = true;
+	}
+
+	public void Explode()
+    {
+		Debug.Log("Explode");
+		if (explosionCount < 3)
+        {
+            if (Time.time >= nextExplosion)
+            {
+                ee.Play(true);
+                nextExplosion = Time.time + 2f;
+                explosionCount++;
+            }
+        }
+        else
+        {
+            isExploding = false;
+            explosionCount = 0;
+        }
+    }
+
+	#endregion
+
+	#region HighSpeed
+
+	public void OnEntryHighSpeed()
+	{
+		isHighSpeed = true;
+		int rand = Random.Range(0, 2);
+		if (rand == 1)
+		{
+			nextHole = Random.Range(0, LHoles.Count - 1);
+			selectedHole = LHoles[nextHole];
+			left = true;
+		}
+		else if (rand == 0)
+		{
+			nextHole = Random.Range(0, RHoles.Count - 1);
+			selectedHole = RHoles[nextHole];
+			left = false;
+		}
+
+		endHS = false;
+		randVoltas = Random.Range(2, 5);
+		voltasCount = 0;
+		voltas = randVoltas + 1;
+		Debug.Log(voltas);
+	}
+
+	public void HighSpeed()
+    {
+		if (Vector3.Distance(transform.position, caveCenter.position) <= 5f  && endHS == true)
+		{
+			isHighSpeed = false;
+			return;
+		}
+
+		Transform previousHole = selectedHole; 
+        if (Vector3.Distance(transform.position, selectedHole.position) <= 5f)
+        {
+            //Look for another hole
+            if (left && !goingIn)
+            {
+				voltasCount++;
+				if (voltasCount == voltas)
+				{
+					selectedHole = caveCenter;
+					endHS = true;
+				}
+				else
+				{
+					left = true;
+					goingIn = true;
+					while (selectedHole == previousHole)
+					{
+						nextHole = Random.Range(0, LHoles.Count - 1);
+						selectedHole = LHoles[nextHole];
+					}
+				}
+            }
+            else if(!left && !goingIn)
+            {
+				voltasCount++;
+				if (voltasCount == voltas)
+				{
+					selectedHole = caveCenter;
+					endHS = true;
+				}
+				else
+				{
+					left = false;
+					goingIn = true;
+					while (selectedHole == previousHole)
+					{
+						nextHole = Random.Range(0, RHoles.Count - 1);
+						selectedHole = RHoles[nextHole];
+					}
+				}
+            }
+            else if(left && goingIn)
+            {
+                goingIn = false;
+                left = false;
+                while (selectedHole == previousHole)
+                {
+                    nextHole = Random.Range(0, RHoles.Count - 1);
+                    selectedHole = RHoles[nextHole];
+                }
+            }
+            else if (!left && goingIn)
+            {
+                goingIn = false;
+                left = true;
+                while (selectedHole == previousHole)
+                {
+                    nextHole = Random.Range(0, LHoles.Count - 1);
+                    selectedHole = LHoles[nextHole];
+                }
+            }
+			Debug.Log(voltasCount);
+		}
+
+		//Movement
+        transform.position += transform.forward *( Time.deltaTime * eelSpeed);
+
+        direction = transform.position - selectedHole.position;
+        Quaternion targetRot = Quaternion.LookRotation(-direction, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRot, 0.04f);
+    }
+
+	#endregion
+
+	#region Calling
+
+	public void OnEntryCalling()
+	{
+		isCalling = true;
+	}
+
+	public void Calling() // change position to spawnEnemyPosition
+    {
+		Debug.Log("Calling");
+		foreach (Transform hole in RHoles)
+        {
+            for(int i=0;i<=enemiesPerHole;i++)
+                Instantiate(enemyBabies,hole.position,Quaternion.identity);
+        }
+
+        foreach (Transform hole in LHoles)
+        {
+            for (int i = 0; i <= enemiesPerHole; i++)
+                Instantiate(enemyBabies, hole.position, Quaternion.identity);
+        }
+
+		isCalling = false;
+    }
+	#endregion
+
+	#region Charge
+
+	public void OnEntryCharge()
+	{
+		isCharging = true;
+		if (!sameCharge)
+		{
+			currentHealth = this.GetComponent<HealthEnemy>().health;
+			sameCharge = true;
+			playerIndex = Random.Range(0, 4);
+		}
+	}
+
+	public void Charge()// talvez procurar os q estam com stun e dar charge a um deles
+	{
+		Debug.Log("Charge");
+		Transform chosenTarget;
+		chosenTarget = _playerList[playerIndex];
+
+		transform.position = Vector3.MoveTowards(transform.position, chosenTarget.position, eelSpeed * Time.deltaTime);
+
+		direction = transform.position - chosenTarget.position;
+		Quaternion targetRot = Quaternion.LookRotation(-direction, Vector3.up);
+		transform.rotation = Quaternion.Lerp(transform.rotation, targetRot, 0.04f);
+	}
+
+	#endregion
+
+	#region Bite
+
+	public void OnEntryBite()
+	{
+		isBiting = true;
+		mouthClosed = true;
+	}
+
+	public void Bite()
+    {
+		Debug.Log("Bite");
+		if (mouthClosed)
+            BottomMouth.localRotation = Quaternion.RotateTowards(BottomMouth.rotation, openMouth , Time.deltaTime * 8f);
+		else
+			BottomMouth.localRotation = Quaternion.RotateTowards(BottomMouth.rotation, closeMouth, Time.deltaTime * 8f);
+
+		if (openMouth == BottomMouth.rotation)
+		{
+			mouthClosed = false;
+			inBiteRange.GetComponent<HealthPlayer>().TakeDamage(BiteDamage);
+		}
+		else if (closeMouth == BottomMouth.rotation)
+		{
+			mouthClosed = true;
+			isBiting = false;
+		}
+	}
+
+	#endregion
+
+	#region ShockWave
+
+	public void OnEntryShockWave()
+	{
+		isShockWave = true;
+	}
+
+	public void ShockWave()
+	{
+		Debug.Log("ShockWave");
+		if (waveCount < 6)
+		{
+			if(Time.time >= nextWave)
+			{
+				SW.Play(true);
+				nextWave = Time.time + 2f;
+				waveCount++;
+			}
+		}
+		else
+		{
+			waveCount = 0;
+			isShockWave = false;
+		}
+	}
+
+	#endregion
+
+	public void AddHoles()
+    {
+        GameObject rholes = GameObject.Find("2Room").transform.Find("Room").transform.Find("RightHoles").gameObject;
+        GameObject lholes = GameObject.Find("2Room").transform.Find("Room").transform.Find("LeftHoles").gameObject;
+
+        foreach (Transform child in rholes.transform)
+        {
+            RHoles.Add(child);
+        }
+        foreach (Transform child in lholes.transform)
+        {
+            LHoles.Add(child);
+        }
+    }
+
+	public float MedPlayersPositions(Vector3 posReference)
+	{
+		float soma = 0.0f;
+		foreach(Transform player in _playerList)
+		{
+			float dist = Vector3.Distance(posReference, player.position);
+			soma += dist;
+		}
+
+		return soma / (_playerList.Count - 1);
+	}
+
+	public int RandNum()
+	{
+		return Random.Range(0, 2);
+	}
+
+	//Collider da boca
+	public void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			isBiting = true;
+			inBiteRange = other.transform;
+		}
+	}
+}
