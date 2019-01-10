068b781ac423c5ac1d34be42b6fcffe2830d348d
xfilipefx <xfilipefx@gmail.com>
2019-01-10 22:41:31 +0000

    FOKKUSU ATENTO

--- Final Descent/Assets/Spawner/SpawnerBehaviour.cs
+++ Final Descent/Assets/Spawner/SpawnerBehaviour.cs
@@ -80,7 +80,7 @@ public class SpawnerBehaviour : MonoBehaviour
             }
             else
             {
-                Destroy(gameObject);
+                //Destroy(gameObject);
             }
         }
     }
